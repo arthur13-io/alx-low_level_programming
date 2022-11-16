abbfdecc96da1c8a5b4a4f4b63e600a094628b54
David Arthur <Davart776@gmail.com>
2022-11-16 14:03:08 -0800

    function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- function pointer that prints a name
+ * Return: success 0
+ */
+void print_name(char *name)
+{
+	printf("%s\n", name);
+}
+int main(void)
+{
+	void (*pointer)(char *);
+
+	pointer = &print_name;
+	pointer("David");
+	return (0);
+}
